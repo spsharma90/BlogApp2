a0378d2ce0cd74f2148ace2aa5ede1d486be1eaf
Shubham <Shubhamse90@gmail.com>
2025-01-03 13:53:48 +0530

    Final Commit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
