8aaba69e3c52eaf95b0208ba69fd3d0370fa374d
spsharma90 <48118234+spsharma90@users.noreply.github.com>
2026-02-02 10:10:57 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
